52d1e9c56e4cd6f8396f865729c0858f3e69fc0b
katehcooh <katarzyna.hejnowicz@gmail.com>
2017-10-21 16:04:31 +0200

    task 2 - display properties

--- js/app.js
+++ js/app.js
@@ -12,6 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
         dropdownList.style.display = 'none';
     });
  //koniec zadania nr 1   
-
-
+    
+//zadanie nr 2
+    var buttons = document.querySelectorAll('.btn.read-more');
+       
+    
+       
+        function showHide () {
+        var textArea = this.previousElementSibling; 
+        
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+            textArea.style.display = 'none'
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }        
+        };
+    
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', showHide);
+        }
+//koniec zadania nr 2    
+    
 });
